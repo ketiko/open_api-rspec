29d4ab687d9b284ff350479c230d909f5b17ff00
Ryan Hansen <ryan.hansen@octanner.com>
2019-02-16 12:43:35 -0700

    Speed up shared example by reducing number of "it" blocks
    Since these are used on request specs, which are not very fast, we want
    to do what we can to not add more lag.
    
    Reduce the number of times the spec has to be re-run for each "it"
    block.
    
    Instead have a single block with multiple expectations.

--- lib/open_api/rspec/shared_examples.rb
+++ lib/open_api/rspec/shared_examples.rb
@@ -7,72 +7,49 @@ module OpenApi
         let(:schema_hash) { ::JSON.parse(open_api_json) }
         let(:schema_parser) { OpenApi::RSpec::SchemaParser.new(schema_hash, request, response) }
 
-        it 'has openapi documentation for url' do
+        it 'matches the swagger.json for the request' do
           expect(schema_parser.schema_for_url).not_to(
             be_nil,
-            'url not documented'
+            'url not documented in swagger.json'
           )
-        end
-
-        it 'matches an allowed http request method' do
           expect(schema_parser.schema_for_url_and_request_method).not_to(
             be_nil,
             'http method for url not documented'
           )
-        end
-
-        it 'has all required request query parameters' do
           schema_parser.openapi_required_query_string_params.each do |openapi_param|
             expect(schema_parser.request_params).to(
               include(openapi_param),
               'missing required request query parameters'
             )
           end
-        end
-
-        it 'has all required request path parameters' do
           schema_parser.openapi_required_path_params.each do |openapi_param|
             expect(schema_parser.request_path_params).to(
               include(openapi_param),
               'missing required request path parameters'
             )
           end
-        end
-
-        it 'has all required request form data parameters' do
           schema_parser.openapi_required_form_data_params.each do |openapi_param|
             expect(schema_parser.request_params).to(
               include(openapi_param),
               'missing required request form data parameters'
             )
           end
-        end
-
-        it 'does not allow undocumented request path parameters' do
           schema_parser.request_path_params.each do |request_param|
             expect(schema_parser.openapi_path_params).to(
               include(request_param),
               "#{request_param} is an undocumented request path parameter"
             )
           end
-        end
-
-        it 'does not allow undocumented request parameters' do
           schema_parser.request_params.each do |request_param|
             expect(schema_parser.openapi_request_params).to(
               include(request_param),
               "#{request_param} is an undocumented request parameter"
             )
           end
-        end
-
-        it 'matches an allowed http response status' do
           expect(schema_parser.schema_for_url_and_request_method_and_response_status).not_to(
             be_nil,
             'http response status not documented'
           )
-        end
-        it 'matches the response schema' do
           response_schema = schema_parser.schema_for_url_and_request_method_and_response_status
           results = if response_schema['schema']
                       ::OpenApi::SchemaValidator.validate_schema!(
